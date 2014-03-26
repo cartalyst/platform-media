79c9a577a976d1bc5cbdc644d3f09a10a16a045b
Bruno Gaspar <brunofgaspar1@gmail.com>
2014-03-26 00:26:38 +0000

    remove old grid views
    Signed-off-by: Bruno Gaspar <brunofgaspar1@gmail.com>

--- themes/admin/default/packages/platform/media/views/data-grid_no-results-tmpl.blade.php
+++
@@ -1,9 +0,0 @@
-<script type="text/template" data-grid="main" id="no-results-tmpl" data-template="no-results">
-	<div class="well">
-		<p></p>
-		<p></p>
-		<p>{{{ trans('table.no_results') }}}</p>
-		<p></p>
-		<p></p>
-	</div>
-</script>
