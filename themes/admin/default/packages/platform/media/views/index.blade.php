6c1158f232021e7b8d0f60c61b3abd4a82280f63
RickvdP <rick95ster@gmail.com>
2014-04-06 18:19:26 +0200

    Replaced static text with language string

--- themes/admin/default/packages/platform/media/views/index.blade.php
+++ themes/admin/default/packages/platform/media/views/index.blade.php
@@ -231,7 +231,7 @@ tr { cursor: default; }
 					{{-- CSRF Token --}}
 					<input type="hidden" name="_token" value="{{ csrf_token() }}">
 
-					<select placeholder="Chose your media tags." id="tags" name="tags[]" multiple="multiple" tabindex="-1"></select>
+					<select placeholder="{{{ trans('platform/media::form.tags_help') }}}" id="tags" name="tags[]" multiple="multiple" tabindex="-1"></select>
 
 					<div class="dz-default dz-message"></div>
 
