fa5e881be2c93f8d19f8e57d81bf8e3326c149a1
Daniel Berry <daniel@danielberry.me>
2014-06-13 14:49:50 -0500

    adds configurable default subject line
    Will be used if no other subject is specified.

--- config/config.php
+++ config/config.php
@@ -10,6 +10,9 @@ return [
 
 		// Limit of the attachments that we'll be sending on the email
 		'attachments_max_size' => 10485760, // 10 mb
+		
+		// default subject line to use if none was specified
+		'subject' => 'You\'ve Got Media!'
 
 	],
 
