7c1b6e60b9175fb7b161c49ca23ba131bf6e94f9
Daniel Berry <daniel@danielberry.me>
2014-06-13 15:09:24 -0500

    adds lang file for email view

---
+++ lang/en/email.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Part of the Platform Media extension.
+ *
+ * NOTICE OF LICENSE
+ *
+ * Licensed under the Cartalyst PSL License.
+ *
+ * This source file is subject to the Cartalyst PSL License that is
+ * bundled with this package in the license.txt file.
+ *
+ * @package    Platform Media extension
+ * @version    2.0.0
+ * @author     Cartalyst LLC
+ * @license    Cartalyst PSL
+ * @copyright  (c) 2011-2014, Cartalyst LLC
+ * @link       http://cartalyst.com
+ */
+
+return [
+	
+	'attachments' => 'Attachments',
+	'total_bytes' => 'Total',
+	
+	'users'       => 'Users',
+	'users_help'  => 'Select users to add as recipients.'
+	
+	'groups'      => 'Groups',
+	'groups_help' => 'Select groups to add as recipients'
+];
