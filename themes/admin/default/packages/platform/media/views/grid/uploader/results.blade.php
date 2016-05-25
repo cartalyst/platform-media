f8b74a3d20ff8e1b50b98bd13ca2c3d484eb134d
Suhayb Wardany <me@suw.me>
2016-05-25 16:35:00 +0200

    use full thumbnail url on uploader widget
    Signed-off-by: Suhayb Wardany <me@suw.me>

--- themes/admin/default/packages/platform/media/views/grid/uploader/results.blade.php
+++ themes/admin/default/packages/platform/media/views/grid/uploader/results.blade.php
@@ -2,9 +2,9 @@
 
 	<% _.each(results, function(r) { %>
         <div class="media-item" data-grid-row>
-            <input id="<%= r.id %>" data-grid-checkbox type="checkbox" name="row[]" value="<%= r.id %>" data-name="<%= r.name %>" data-thumbnail="<%= r.thumbnail %>">
+            <input id="<%= r.id %>" data-grid-checkbox type="checkbox" name="row[]" value="<%= r.id %>" data-name="<%= r.name %>" data-thumbnail="<%= r.thumbnail_uri %>">
             <label for="<%= r.id %>">
-                <div class="media-img" style="background-image: url('<%= r.thumbnail %>')"></div>
+                <div class="media-img" style="background-image: url('<%= r.thumbnail_uri %>')"></div>
                 <span class="media-title"><a href="<%= r.edit_uri %>"><%= r.name %></a></span>
                 <span class="media-date"><%= moment(r.created_at).format('MMM DD, YYYY') %></span>
             </label>
