064fe340e8bc741a7749662c307751ae85a94a01
Dan Syme <dan.syme@gmail.com>
2016-08-02 05:52:46 -0700

    tidy

--- themes/admin/default/packages/platform/media/views/widgets/upload.blade.php
+++ themes/admin/default/packages/platform/media/views/widgets/upload.blade.php
@@ -24,36 +24,45 @@
 
 @stop
 
-@if ($model)
-<input type="hidden" data-model-id="{{ $model->id }}">
-<input type="hidden" data-object-class="{{ get_class($model) }}">
-@endif
+<div class="media-upload-widget">
 
-<input type="hidden" data-upload-post-url="{{ route('admin.media.link_media') }}">
+    @if ($model)
+    <input type="hidden" data-model-id="{{ $model->id }}">
+    <input type="hidden" data-object-class="{{ get_class($model) }}">
+    @endif
 
+    <input type="hidden" data-upload-post-url="{{ route('admin.media.link_media') }}">
 
-<div class="clearfix">
-    <div class="pull-right mb15">
-        <a class="tip btn btn-primary btn-md" href="#" data-toggle="modal" data-target="#media-modal" data-original-title="{{ trans('action.upload') }}">
+    <ul class="nav nav-default pull-right">
+
+        <a class="tip btn btn-default" href="#" data-toggle="modal" data-target="#media-modal" data-original-title="{{ trans('action.upload') }}">
             <i class="fa fa-upload"></i> {{ trans('action.upload') }}
         </a>
 
-        <a class="tip btn btn-primary btn-md" href="#" data-toggle="modal" data-target="#media-selection-modal" data-original-title="{{ trans('platform/media::action.select') }}">
+        <a class="tip btn btn-default" href="#" data-toggle="modal" data-target="#media-selection-modal" data-original-title="{{ trans('platform/media::action.select') }}">
             <i class="fa fa-list-ul"></i> {{ trans('platform/media::action.select') }}
         </a>
-    </div>
-    <label class="mt5"><i class="fa fa-info"></i> {{ trans('platform/media::widget.title') }}</label>
-</div>
 
-<div class="clearfix">
+    </ul>
+
+    <p class="lead">{{ trans('platform/media::widget.delete_info') }} {{ $multiUpload ? trans('platform/media::widget.multi_upload') : trans('platform/media::widget.single_upload') }}</p>
+
+    <hr>
+
     <input type="hidden" id="mediaArray">
-    <input type="hidden" name="selected_media[]"">
+    <input type="hidden" name="selected_media[]">
+
+    <!-- List group -->
     <ul id="mediaList" class="upload__attachments list-group">
+
         @foreach ($currentUploads as $upload)
+
         <li class="list-group-item clearfix" id="attached_media_{{ $upload->id }}">
 
             <div class="flex-row">
+
                 <div class="list-group-item-left">
+
                     <i class="fa fa-arrows"></i>
 
                     @if ($upload->is_image == 1)
@@ -67,26 +76,28 @@
                     @endif
 
                 </div>
+
                 <div class="list-group-item-center">
                     <span>{{ $upload->name }}</span>
                     <input type="hidden" name="_media_ids[]" value="{{ $upload->id }}">
                 </div>
+
                 <div class="list-group-item-right">
-                    <a href="{{ route('admin.media.edit', [ $upload->id ]) }}" class="btn btn-xs btn-success"><i class="fa fa-fw fa-pencil"></i></a>
-                    <button type="button" class="btn btn-xs btn-danger" data-media-delete><i class="fa fa-fw fa-trash"></i></button>
+                    <a href="{{ route('admin.media.edit', [ $upload->id ]) }}" class="btn btn-sm btn-default"><i class="fa fa-fw fa-pencil"></i></a>
+                    <button type="button" class="btn btn-sm btn-danger" data-media-delete><i class="fa fa-fw fa-trash"></i></button>
                 </div>
+
             </div>
+
             <div class="overlay">
                 <i class="fa fa-spinner fa-spin"></i>
             </div>
 
         </li>
+
         @endforeach
-    </ul>
 
-    <p><small><i class="fa fa-info"></i>&nbsp; {{ trans('platform/media::widget.delete_info') }}</small></p>
-
-    <p><small><i class="fa fa-info"></i>&nbsp; {{ $multiUpload ? trans('platform/media::widget.multi_upload') : trans('platform/media::widget.single_upload') }}</small></p>
+</ul>
 
 </div>
 
@@ -94,8 +105,11 @@
 @include('platform/media::selection-modal')
 
 <script type="text/template" data-media-attachment-template>
+
     <li class="list-group-item clearfix" id="attached_media_<%= media.id %>">
+
         <div class="flex-row">
+
             <div class="list-group-item-left">
                 <i class="fa fa-arrows"></i>
                 <% if (media.is_image == 1) { %>
@@ -108,18 +122,24 @@
                 <div class="selected-media-img" style="background-image:url('{{ Asset::getUrl('platform/media::img/other.png') }}')"></div>
                 <% } %>
             </div>
+
             <div class="list-group-item-center">
                 <span><%- media.name %></span>
                 <input type="hidden" name="_media_ids[]" value="<%= media.id %>">
             </div>
+
             <div class="list-group-item-right">
                 <a href="<%- '{{ route('admin.media.edit', [ 'id' ]) }}'.replace('id', media.id) %>" class="btn btn-xs btn-success"><i class="fa fa-fw fa-pencil"></i></a>
 
                 <button type="button" class="btn btn-xs btn-danger" data-media-delete><i class="fa fa-fw fa-trash"></i></button>
             </div>
+
         </div>
+
         <div class="overlay">
             <i class="fa fa-spinner fa-spin"></i>
         </div>
+
     </li>
+
 </script>
