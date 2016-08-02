e3699cb26359f22cf3d5cb00a7c721dbebdd069a
Bruno Gaspar <brunofgaspar1@gmail.com>
2016-08-02 11:43:52 +0100

    Remove thumbnail column migration.
    Signed-off-by: Bruno Gaspar <brunofgaspar1@gmail.com>

---
+++ database/migrations/2016_08_02_114112_migration_platform_media_remove_thumbnail_column.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * Part of the Platform Media extension.
+ *
+ * NOTICE OF LICENSE
+ *
+ * Licensed under the Cartalyst PSL License.
+ *
+ * This source file is subject to the Cartalyst PSL License that is
+ * bundled with this package in the LICENSE file.
+ *
+ * @package    Platform Media extension
+ * @version    4.0.0
+ * @author     Cartalyst LLC
+ * @license    Cartalyst PSL
+ * @copyright  (c) 2011-2016, Cartalyst LLC
+ * @link       http://cartalyst.com
+ */
+
+use Illuminate\Database\Migrations\Migration;
+
+class MigrationPlatformMediaRemoveThumbnailColumn extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('media', function ($table) {
+            $table->dropColumn('thumbnail');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('media', function ($table) {
+            $table->text('thumbnail')->before('width')->nullable();
+        });
+    }
+}
